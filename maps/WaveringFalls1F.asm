0852fb7822fcca77f515adac372e63580a287697
MyCleaningSupplies <abdelouzzine@gmail.com>
2021-02-07 03:11:57 +0000

    Remade WaveringFalls1F and edited WaveringFalls Events

--- maps/WaveringFalls1F.asm
+++ maps/WaveringFalls1F.asm
@@ -9,7 +9,7 @@ WaveringFalls1F_MapEvents:
     db 0,0 ; filler
 
     def_warp_events
-    warp_event 13, 16, WAVERING_FALLS_OUTSIDE, 1
+    warp_event 27, 1, WAVERING_FALLS_OUTSIDE, 2
 
     def_coord_events
 
